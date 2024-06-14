89bbc4119a4df3cbf4d5cef0a15ac7c258224a5d
Vanasik <132499152+Vanasik@users.noreply.github.com>
2024-06-14 04:20:27 +0300

    feat:add startnowclick.js

---
+++ startnowclick.js
@@ -0,0 +1,46 @@
+document.querySelector('.startnow').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.body.classList.add('fade-out');
+
+
+    
+    setTimeout(function() {
+        window.location.href = '../registrform/registrationform.html';
+    }, 20); // Должно соответствовать продолжительности перехода в CSS
+});
+
+document.querySelector('.dif').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.body.classList.add('fade-out');
+
+
+    
+    setTimeout(function() {
+        window.location.href = '../registrform/registrationform.html';
+    }, 20); // Должно соответствовать продолжительности перехода в CSS
+});
+
+document.querySelector('.big').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.body.classList.add('fade-out');
+
+
+    
+    setTimeout(function() {
+        window.location.href = '../registrform/registrationform.html';
+    }, 20); // Должно соответствовать продолжительности перехода в CSS
+});
+
+document.querySelector('.dif9').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.body.classList.add('fade-out');
+
+
+    
+    setTimeout(function() {
+        window.location.href = '../registrform/registrationform.html';
+    }, 20); // Должно соответствовать продолжительности перехода в CSS
+});
+
+
+
